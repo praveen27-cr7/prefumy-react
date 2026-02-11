7c999c654feab830b18bf9a079abe1482d19379b
praveen27-cr7 <praveen270304@gmail.com>
2026-02-11 23:27:23 +0530

    update prj

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import reactDom from 'react-dom/client'
-import "./style.css"
+import ReactDom from 'react-dom/client';
+import "./style.css";
 import App from './App';
 
-const root = reactDom.createRoot(document.getElementById("root"))
+const root = ReactDom.createRoot(document.getElementById("root"))
 
 
 root.render(<div>
